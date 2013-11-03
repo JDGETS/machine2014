24acc47fce4a8d6ef1eed7c0c4199a86122bd91a
Israel Hallé <isra017@gmail.com>
2013-11-03 18:58:42 -0500

    Fixed state bug

--- code/src/lib/state.py
+++ code/src/lib/state.py
@@ -1,4 +1,5 @@
 import time
+from functools import partial
 
 class State:
     def update(self):
@@ -7,13 +8,18 @@ class State:
             if state != None:
                 self.state = state()
         except StopIteration:
+            print '[State.update] state machine done'
 
     def set_state(self, state):
         self.state = state
 
     def wait(self, delay, next_state):
+        return partial(self.wait_state, delay, next_state)
+
+    def wait_state(self, delay, next_state):
         start_time = time.clock()
-        while(time.clock() - start_time < delay_ms):
+        while(time.clock() - start_time < delay):
             yield
 
         yield next_state
+
--- code/tests/sorter.py
+++ code/tests/sorter.py
@@ -1,5 +1,22 @@
 from collector import Sorter
 
+import sys
+
+def info(type, value, tb):
+   if hasattr(sys, 'ps1') or not sys.stderr.isatty():
+      # we are in interactive mode or we don't have a tty-like
+      # device, so we call the default hook
+      sys.__excepthook__(type, value, tb)
+   else:
+      import traceback, pdb
+      # we are NOT in interactive mode, print the exception...
+      traceback.print_exception(type, value, tb)
+      print
+      # ...then start the debugger in post-mortem mode.
+      pdb.pm()
+
+sys.excepthook = info
+
 sorter = Sorter()
 sorter.start()
 while True:
