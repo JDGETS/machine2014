07d3034d9a706e00235be552c6be73a71519751c
Israel Hallé <isra017@gmail.com>
2013-11-07 22:34:26 -0500

    Update servo driver

--- code/src/device/servo.py
+++ code/src/device/servo.py
@@ -1,16 +1,18 @@
 import Adafruit_BBIO.PWM as PWM
 
 class Servo:
-    DUTY = 1950
-
+    DUTY_MIN = 1120
+    DUTY_MAX = 1920
+    ANGLE_MAX = 90
     def __init__(self, pin):
         self.pin = pin
-        # PWM.start(self.pin, self.DUTY)
+        PWM.start(self.pin, self.DUTY_MAX, freq=50)
 
-    def set_angle(self, duty_cycle):
-        # PWM.set_duty_cycle(self.pin, duty_cycle)
+    def set_angle(self, angle):
+        duty = angle/self.ANGLE_MAX. * (self.DUTY_MAX-self.DUTY_MIN) + self.DUTY_MIN
+        PWM.set_duty_cycle(self.pin, duty)
         return
 
     def stop(self):
-        # PWM.stop(self.pin)
+        PWM.stop(self.pin)
         PWM.cleanup()
