fd18ce06844b0df3e486d3b6cddfd383f4ba44e5
Israel Hallé <isra017@gmail.com>
2013-11-26 19:46:15 -0500

    Added config value for servo

--- code/src/device/color_sensor.py
+++ code/src/device/color_sensor.py
@@ -1,10 +1,15 @@
+import Adafruit_BBIO.ADC as ADC
+
+ADC.setup()
+
 class ColorSensor:
     UNKOWN = 0
     WHITE = 1
     ORANGE = 2
 
-    def __init__(self, pin):
+    def __init__(self, r_pin, g_pin, b_pin, white_val, orange_val, error):
         pass
 
     def get_color(self):
         return self.UNKOWN
+
--- code/src/lib/config.py
+++ code/src/lib/config.py
@@ -3,16 +3,24 @@ devices = {
         "pin": "P0_00"
     },
     "white_piston": {
-        "pin": "P0_00",
-        "pull_duty": 4,
-        "push_duty": 14
+        "pin": "P9_14",
+        "pull_duty": 9.4,
+        "standby_duty": 10.5,
+        "push_duty": 11.5
     },
-    "white_piston": {
-        "pin": "P0_00"
-        "pull_duty": 4,
-        "push_duty": 14
+    "orange_piston": {
+        "pin": "P9_22"
+        "pull_duty": 5.5,
+        "standby_duty": 7,
+        "push_duty": 7.7
     },
     "color_sensor": {
-        "pin": "P0_00"
+        "r_pin": "P9_33",
+        "g_pin": "P9_35",
+        "b_pin": "P9_36",
+        "white_val": [255,255,255],
+        "orange_val": [255, 255, 0],
+        "error": 128
     }
 }
+
