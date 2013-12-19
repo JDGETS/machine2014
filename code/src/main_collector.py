91dad2e6b3da5b658303441fac80937952b70191
Mathieu Lavoie <mathieulavoie020@gmail.com>
2013-12-18 23:57:39 -0500

    Ninja

--- code/src/main_collector.py
+++ code/src/main_collector.py
@@ -4,14 +4,14 @@ import sys
 
 
 def mainSafe():
-    try:
-        collector = Collector()
-        collector.run()
+    #try:
+    collector = Collector()
+    collector.run()
 
-    except:
-        print "Exception raised  in maincollector::mainSafe", sys.exc_info()[0]
-        collector.stop()
-        raise
+    #except:
+    #    print "Exception raised  in maincollector::mainSafe", sys.exc_info()[0]
+    #    collector.stop()
+    #    raise
 
 if __name__ == "__main__":
     mainSafe();
