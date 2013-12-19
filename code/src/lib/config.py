b8132e73172bf3cbe775e46c9f3bc736fe0a6214
Mathieu Binette <mbinette91@gmail.com>
2013-12-19 17:53:00 -0500

    Oublié la ,

--- code/src/lib/config.py
+++ code/src/lib/config.py
@@ -4,13 +4,13 @@ devices = {
     },
     "white_piston": {
         "pin": "P9_22", #Servo1
-        "pull_duty": 4.75,
+        "pull_duty": 4.6,#TODO CALIBRATE
         "standby_duty": 6.3,
         "push_duty": 7.5
     },
     "orange_piston": {
         "pin": "P9_14", #Servo0
-        "pull_duty": 4.6,
+        "pull_duty": 4.5,
         "standby_duty": 6.2,
         "push_duty": 7.7
     },
@@ -18,9 +18,10 @@ devices = {
         "a_pin": "P9_39", #RGB1
         "b_pin": "P9_40",
         "c_pin": "P9_37",
-        "black_val": [0.010644444115459919, 0.014177777767181397, 0.022111111506819724],
-        "white_val": [0.065244445204734797, 0.064688889533281321, 0.098844445347785956],
-        "orange_val": [0.023955555856227873, 0.073466666638851166, 0.078111109435558324]
+        "black_val": [0.020,0.180,0.027],
+        "white_val": [0.078,0.230,0.12],
+        "orange_val": [0.077,0.22,0.085],
+        "error": 0.02
     }
     ,
      "vacuum_servo": {
@@ -34,7 +35,7 @@ devices = {
         "pull_duty": 9.4, #TODO CALIBRATE
         "standby_duty": 10.5,
         "push_duty": 11.5
-    }
+    },
     "collector_on_switch": {
         "pin": "P8_16", #Switch 
     }
