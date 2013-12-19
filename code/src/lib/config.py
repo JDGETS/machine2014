c531698245e2d26eec371976cfaffc600ff4d003
Mathieu Lavoie <mathieulavoie020@gmail.com>
2013-12-18 21:25:59 -0500

    Update Config

--- code/src/lib/config.py
+++ code/src/lib/config.py
@@ -1,27 +1,40 @@
 devices = {
     "vacuum": {
-        "pin": "P0_00"
+        "pin": "P9_15" # Out 1
     },
     "white_piston": {
-        "pin": "P9_22",
-        "pull_duty": 9.4,
+        "pin": "P9_22", #Servo1
+        "pull_duty": 9.4,#TODO CALIBRATE
         "standby_duty": 10.5,
         "push_duty": 11.5
     },
     "orange_piston": {
-        "pin": "P9_14",
+        "pin": "P9_14", #Servo0
         "pull_duty": 5.5,
         "standby_duty": 7,
         "push_duty": 7.7
     },
     "color_sensor": {
-        "a_pin": "P9_33",
+        "a_pin": "P9_33", #RGB1
         "b_pin": "P9_35",
         "c_pin": "P9_36",
         "black_val": [0.020,0.180,0.027],
         "white_val": [0.078,0.230,0.12],
         "orange_val": [0.077,0.22,0.085],
         "error": 0.02
+    }
+    ,
+     "vacuum_servo": {
+        "pin": "P9_42", #Servo3
+        "pull_duty": 9.4, #TODO CALIBRATE
+        "standby_duty": 10.5,
+        "push_duty": 11.5
+    },
+    "gate_servo": {
+        "pin": "P8_13", #Servo2
+        "pull_duty": 9.4, #TODO CALIBRATE
+        "standby_duty": 10.5,
+        "push_duty": 11.5
     }
 }
 
