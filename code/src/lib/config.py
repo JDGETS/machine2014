a5b72d6e08ccffbebe8b91987ce47c36f3be644f
Mathieu Lavoie <mathieulavoie020@gmail.com>
2013-12-19 00:36:12 -0500

    test

--- code/src/lib/config.py
+++ code/src/lib/config.py
@@ -6,13 +6,13 @@ devices = {
         "pin": "P9_22", #Servo1
         "pull_duty": 4.6,#TODO CALIBRATE
         "standby_duty": 6.3,
-        "push_duty": 7.5
+        "push_duty": 7
     },
     "orange_piston": {
         "pin": "P9_14", #Servo0
         "pull_duty": 4.5,
         "standby_duty": 6.2,
-        "push_duty": 7.7
+        "push_duty": 7
     },
     "color_sensor": {
         "a_pin": "P9_33", #RGB1
@@ -26,9 +26,9 @@ devices = {
     ,
      "vacuum_servo": {
         "pin": "P9_42", #Servo3
-        "pull_duty": 9.4, #TODO CALIBRATE
-        "standby_duty": 10.5,
-        "push_duty": 11.5
+        "pull_duty": 4.5, #TODO CALIBRATE
+        "standby_duty": 4.5,
+        "push_duty": 4.5
     },
     "gate_servo": {
         "pin": "P8_13", #Servo2
