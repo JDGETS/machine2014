aa77dd514e844cb308c0016ae6d5933072abb496
Mathieu Lavoie <mathieulavoie020@gmail.com>
2013-12-20 18:47:14 -0500

    ninja

--- code/src/tests/stepper_rail.py
+++ code/src/tests/stepper_rail.py
@@ -1,7 +1,7 @@
 import lib.config
 from device.stepper import *
 
-stepper = Stepper(lib.config.devices["stepper_rail"])
+stepper = Stepper(**lib.config.devices["stepper_rail"])
 while True:
     steps = int(raw_input("steps: "))
     direction = int(raw_input("direction (0 or 1):"))
