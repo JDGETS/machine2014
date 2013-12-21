9b6848d27d4f405f835be266f033d77c490bf391
Mathieu Binette <mbinette91@gmail.com>
2013-12-20 20:52:20 -0500

    Code du camion

--- code/src/camion/camion.py
+++ code/src/camion/camion.py
@@ -1,67 +1,64 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 import time
-
-from comm import CommCamion
-from device import LimitSwitch
+from device.switch import Switch
+from lib import config
 
 class Camion:
-    """ Contient la logique du Camion (voir séquence Collector sur Google Drive) """
 
-    COLLECTOR_SWITCH_ID = "foo1"
-    DUMP_SWITCH_ID = "foo2"
+    CAMION_CONFIG_ID = "camion"
+    COLLECTOR_SWITCH_ID = "camion_collector_switch"
+    DUMP_SWITCH_ID = "camion_dump_switch"
+    FOOT_STEPPER_ID = "camion_stepper"
 
     def __init__(self):
-        print "Init Camion()"
-        self.comm = CommCamion()
-        self.comm.add_callback(self.receiveComm)
-        #Présence camion devant trieuse et devant dump (pour dumper les balles)
-        self.switchCollector = LimitSwitch(self.COLLECTOR_SWITCH_ID, self.updateSwitchCollector)
-        self.switchDump = LimitSwitch(self.DUMP_SWITCH_ID, self.updateSwitchDump)
+        print "[Camion.__init__]"
+        self.config = **config.devices[self.CAMION_CONFIG_ID]
+        self.collector_switch = Switch(**config.devices[self.COLLECTOR_SWITCH_ID])
+        self.dump_switch = Switch(**config.devices[self.DUMP_SWITCH_ID]) # Useless for now
+        self.foot_stepper = Stepper(**config.devices[self.FOOT_STEPPER_ID])
 
-    def start(self):
-        pass
+    def run(self):
+        print "[Camion.run] Start camion - waiting for signal"
 
-    def stop(self):
-        pass
+        self.wait_for_signal();
 
-    def receiveComm(self, event):
-        if event.message == "START":
-            print "Camion: Starting the collector (pushing the limitswitch)"
-            self.startCollector()
+        print "[Camion.run] Camion started"
+        
+        self.put_in_start_position();
 
-    def updateSwitchCollector(self, event):
-        if event.activated:
-            self.dropWeight()
-        else:
-            self.retrieveWeight()
+        self.is_running = True
+        self.first_run = True
+        while self.is_running:
+            if not (self.first_run and self.collector_switch.is_pressed()):
+                self.collector_switch.wait_pushed()
 
-    def updateSwitchDump(self, event):
-        if event.activated:
-            self.dropWeight()
-            #wait...
-            self.releaseBalls()
-            #wait...
-            self.retrieveWeight()
-        else:
-            self.holdBalls()
+            self.drop_foot();
 
-    def startCollector(self):
-        """ Hit the switch on the collector to grab the Camion. """
-        pass
+            time.sleep( self.config.foot_standby_time )
+            
+            self.bring_foot_up();
+            
+            self.first_run = False
 
-    def dropWeight(self):
-        """ Lâcher le poid. """
-        pass
+        print "[Camion.run] Camion stopped"
+        
+    def wait_for_signal(self):
+        """Wait for signal here"""
+        #while ...:
+        #    pass
+        return
 
-    def retrieveWeight(self):
-        """ Remonter le poids. """
-        pass
+    def put_in_start_position(self):
+        #Drop camion. Foot on floor so bring it up.
+        stepper.move(self.config.stepper_start_position_ticks, 1)
+        return
 
-    def releaseBalls(self):
-        """ Relacher les balles dans la zone dump. """
-        pass
+    def drop_foot(self):
+        stepper.move(self.config.stepper_foot_complete_ticks, 0)
+        #If the switch isnt on, continue bringing it up a few ticks (5%) at the time
 
-    def holdBalls(self):
-        """ Refermer le container. """
-        pass
+    def bring_foot_up(self):
+        stepper.move(self.config.stepper_foot_complete_ticks, 1)
+        #If the switch isnt on, continue bringing it up a few ticks (5%) at the time
+        
