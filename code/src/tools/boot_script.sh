e341321fedc742bedee52b775542c75e7ae6151d
Machine <machine@machine.com>
2013-12-21 01:25:44 +0000

    Added systemd service file

--- code/src/tools/boot_script.sh
+++ code/src/tools/boot_script.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-/home/root/machine2014/code/src/tools/process_spawner.py P8_12 /home/root/machine2014/code/src/spawn_collector.py
+DIR=/home/root/machine2014/code/src
+export PYTHONPATH=$PYTHONPATH:$DIR
+$DIR/tools/process_spawner.py P8_4 $DIR/spawn_collector.py 2>&1 >> $DIR/collector.log
