6fb0788b272ea6034de4a4f6452a3a803d803323
Mathieu Binette <mbinette91@gmail.com>
2013-12-22 14:32:42 -0500

    more intiialize params.

--- code/tests/initialize_collector.py
+++ code/tests/initialize_collector.py
@@ -6,34 +6,24 @@ print "The correct starting state for the collector is: Truck on sort_position. 
 r = Rail()
 gate = Piston(**config.devices["gate_servo"])
 while True:
-    print "1 : Goto Home"
-    print "2 : Goto Waiting for sort position"
-    print "3 : Goto Away (Dump)"
-    print "4 : Close gate"
-    print "5 : Open gate"
-    print "6 : Goto X right (total distance = 17000)"
-    print "7 : Goto X left (total distance = 17000)"
+    print "TOTAL DISTANCE: "+str(r.HOME_POSITION)
+    print "SORTING POSITION: "+str(r.WAIT_FOR_SORTING_POSITION)
+    print "1 : Goto X right"
+    print "2 : Goto X left"
+    print "3 : Close gate"
+    print "4 : Open gate"
     c = int(raw_input("your choice?"))
-
-    if c ==1:
-        print "going home"
-        r.slide_to_home()
+  
+    if c == 1:
+        c = int(raw_input("how much? "))
+        r.stepper.move(0,abs(c))
     elif c == 2:
-        print "going wait"
-        r.slide_to_wait_for_sorting_position()
+        c = int(raw_input("how much? "))
+        r.stepper.move(1,abs(c))
     elif c == 3:
-        print "going dump"
-        r.slide_to_away()
-    elif c == 4:
         print "closing gate"
         gate.push()
-    elif c == 5:
+    elif c == 4:
         print "opening gate"
         gate.pull()
-    elif c == 6:
-        c = int(raw_input("how much?"))
-        r.stepper.move(0,abs(c))
-    elif c == 7:
-        c = int(raw_input("how much?"))
-        r.stepper.move(1,abs(c))
 
