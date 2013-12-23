601c6b6c24fbbbf3c99e6541acafb8fe8186ae7b
Machine <machine@machine.com>
2013-12-23 16:52:06 +0000

    simplify Pisto class

--- code/src/device/piston.py
+++ code/src/device/piston.py
@@ -1,26 +1,22 @@
 from servo import Servo
 
-class Piston(object):
+class Piston(Servo):
     """Piston controller"""
 
     def __init__(self, pin, pull_duty, standby_duty, push_duty):
-        self.pin = pin
+        Servo.__init__(pin, pull_duty)
         self.pull_duty = pull_duty
         self.standby_duty = standby_duty
         self.push_duty = push_duty
-        self.servo = Servo(self.pin, self.pull_duty)
 
     def standby(self):
         """set servo to standby position"""
-        self.servo.set_duty(self.standby_duty)
+        self.set_duty(self.standby_duty)
 
     def push(self):
         """set servo to push position"""
-        self.servo.set_duty(self.push_duty)
+        self.set_duty(self.push_duty)
 
     def pull(self):
         """set servo to pull position"""
-        self.servo.set_duty(self.pull_duty)
-
-    def stop(self):
-        self.servo.stop()
+        self.set_duty(self.pull_duty)
