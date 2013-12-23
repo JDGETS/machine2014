294ee48682f1912192f04f9ea5721a580c7a8ca7
Mathieu Binette <mbinette91@gmail.com>
2013-12-23 05:00:34 -0500

    ninja magntix

--- code/src/device/switch.py
+++ code/src/device/switch.py
@@ -49,6 +49,7 @@ class Switch(object):
         GPIO.wait_for_edge(self.pin, GPIO.RISING)
         return
 
+import time
 class MagneticSwitch(Switch):
 
     def __init__(self, pin):
@@ -58,3 +59,8 @@ class MagneticSwitch(Switch):
         GPIO.setup(pin, GPIO.IN, GPIO.PUD_UP)
         self.do_once = None
         GPIO.add_event_detect(pin, GPIO.RISING, self.do_something)
+    
+    def wait_released(self):
+        """ Wait for the switch to be released (waits for a raising edge). YES. This is important with magnetic switches. And wait_released is already blocking so leave me alone with the time.sleep! """
+        while not self.is_released:
+            time.sleep(0.01)
