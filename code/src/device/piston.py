13a0bc1035a717a96eca631bcd88ac1702d5e603
Mathieu Binette <mbinette91@gmail.com>
2013-12-23 18:51:28 -0500

    ninja wow

--- code/src/device/piston.py
+++ code/src/device/piston.py
@@ -4,7 +4,7 @@ class Piston(Servo):
     """Piston controller"""
 
     def __init__(self, pin, pull_duty, standby_duty, push_duty):
-        Servo.__init__(pin, pull_duty)
+        Servo.__init__(self, pin, pull_duty)
         self.pull_duty = pull_duty
         self.standby_duty = standby_duty
         self.push_duty = push_duty
