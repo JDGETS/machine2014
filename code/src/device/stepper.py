c1c920e38abe1f40eebeec369321a9eb17664164
Mathieu Binette <mbinette91@gmail.com>
2013-12-23 19:11:48 -0500

    Revert thread stuff stepper

--- code/src/device/stepper.py
+++ code/src/device/stepper.py
@@ -2,91 +2,73 @@ from threading import Thread, Event
 import Adafruit_BBIO.GPIO as GPIO
 import bbio
 
-class Stepper(Thread):
+#Must be out of the Stepper object. A Thread can only be started once.
+# OR:
+# """ If you derive a class from threading.Thread you can add a Thread.__init__(self) 
+#     at the end of your run method and you’ll be able to call start again and it’ll 
+#     automatically reinitialize itself when it’s done. """
+def move_thread(kill, pin, steps=-1, default_ramp_step = 2000, stop_condition = None):
+    """ run this biatch """
+    STOP_CONDITION_INTERVAL = 25
+    stop_condition = stop_condition or (lambda: False); #Default: No stop conditions
+    bbio.pinMode(pin, bbio.OUTPUT)
+    step = 0
+    ramp_step =  default_ramp_step if steps == -1 else min(default_ramp_step, steps)
+    ramp_sleep = 100.0
+    ramp_sleep_decrement = ramp_sleep / ramp_step
+    min_sleep = 100 - 32 - 15
 
-    def __init__(self, pin, direction, reset, enable, ramp_step):
-        Thread.__init__(self)
+    while (step < ramp_step) and not kill.isSet() and \ 
+        (step%STOP_CONDITION_INTERVAL != 0 or not stop_condition()):
+        bbio.digitalWrite(pin,bbio.LOW)
+        bbio.digitalWrite(pin,bbio.HIGH)
+        step +=1
+        bbio.delayMicroseconds( min_sleep + ramp_sleep - ramp_sleep_decrement)
 
+    while (step < steps or steps == -1) and not kill.isSet() and \ 
+        (step%STOP_CONDITION_INTERVAL != 0 or not stop_condition()):
+        bbio.digitalWrite(pin,bbio.LOW)
+        bbio.digitalWrite(pin,bbio.HIGH)
+        step +=1
+        bbio.delayMicroseconds(min_sleep)
+
+class Stepper(object):
+    def __init__(self, pin,direction,reset,enable,ramp_step):
         self.default_ramp_step = ramp_step
         self.pin = pin
-        self.kill_evt = Event()
+        self.killThread = Event()
         self.direction = direction
         self.reset = reset
         self.enable = enable
-
-        self.steps = None
-        self.stop_condition = None
-
+        self.thread = None
         GPIO.setup(self.reset, GPIO.OUT)
         GPIO.setup(self.enable, GPIO.OUT)
         GPIO.setup(self.direction, GPIO.OUT)
-
         self.reset_stepper()
 
-    def disable_stepper(self):
-         GPIO.output(self.enable, GPIO.HIGH)
-
     def reset_stepper(self):
-        """ toggle the reset pin and enable the pololu. """
         GPIO.output(self.reset, GPIO.LOW)
         GPIO.output(self.reset, GPIO.HIGH)        
         GPIO.output(self.enable, GPIO.LOW)
 
     #0 or 1 for direction
     def move(self, direction, steps = -1, stop_condition = None):
-        """ give a new move order. reset the thread if needed. """
-
         print "move %d"%steps
-
         GPIO.output(self.direction, direction)
-        if self.is_alive():
+        if self.thread and self.thread.is_alive():
             self.stop()
         
         self.reset_stepper()
-
-        self.steps = steps
-        self.stop_condition = stop_condition or (lambda: False)
-
-        self.start()
+        self.thread = Thread(target = move_thread, args = (self.killThread, self.pin, steps, self.default_ramp_step, stop_condition))
+        self.thread.start()
     
     def is_moving(self):
         return self.thread and self.thread.is_alive()
 
     def stop(self, event = None):
-        """ stop the thread """
-
         print "STOP!!!"
         if self.thread:
-            self.kill_evt.set()
-        
-        self.kill_evt.clear()
+            self.killThread.set()
+            self.thread.join()
+        self.killThread.clear()
         GPIO.output(self.enable, GPIO.HIGH)
-
-    def run(self):
-        """ run this biatch """
-
-        STOP_CONDITION_INTERVAL = 25
-
-        bbio.pinMode(self.pin, bbio.OUTPUT)
-
-        step = 0
-        
-        ramp_step =  self.default_ramp_step if self.steps == -1 else min(self.default_ramp_step, self.steps)
-        ramp_sleep = 100.0
-        ramp_sleep_decrement = ramp_sleep / ramp_step
-        min_sleep = 100 - 32 - 15
-
-        while (step < ramp_step) and not self.kill_evt.isSet() and (step%STOP_CONDITION_INTERVAL != 0 \
-                                                        or not self.stop_condition()):
-            bbio.digitalWrite(self.pin,bbio.LOW)
-            bbio.digitalWrite(self.pin,bbio.HIGH)
-            step +=1
-            bbio.delayMicroseconds( min_sleep + ramp_sleep - ramp_sleep_decrement)
-
-        while (step < self.steps or self.steps == -1) and not self.kill_evt.isSet() and \
-                (step%STOP_CONDITION_INTERVAL != 0 or not self.stop_condition()):
-            bbio.digitalWrite(self.pin,bbio.LOW)
-            bbio.digitalWrite(self.pin,bbio.HIGH)
-            step +=1
-            bbio.delayMicroseconds(min_sleep)
-
