bb811c8ea6fad8af7b15279d9585bd85406777d1
Mathieu Binette <mbinette91@gmail.com>
2013-12-23 19:33:01 -0500

    ninja

--- code/src/main_camion.py
+++ code/src/main_camion.py
@@ -8,13 +8,6 @@ if __name__ == "__main__":
         print "[main_camion] Start camion"
         c = Camion()
         c.run()
-
-    except Exception as e:
-        print "[main_camion] Exception raised in main_camion::mainSafe"
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        print(exc_type, fname, exc_tb.tb_lineno)
-        raise e
         
     finally:
         if c:
