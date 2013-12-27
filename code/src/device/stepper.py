5f768640cc768e67bd8462a3a2902cb6bf88ea17
Mathieu Binette <mbinette91@gmail.com>
2013-12-27 02:09:14 -0500

    asdsrgh.

--- code/src/device/stepper.py
+++ code/src/device/stepper.py
@@ -9,7 +9,7 @@ import bbio
 #     automatically reinitialize itself when it's done. """
 def move_thread(kill, pin, steps=-1, default_ramp_step = 2000, min_sleep = 100-15, stop_condition = None):
     """ run this biatch """
-    STOP_CONDITION_INTERVAL = 2
+    STOP_CONDITION_INTERVAL = 10
     stop_condition = stop_condition or (lambda: False); #Default: No stop conditions
     bbio.pinMode(pin, bbio.OUTPUT)
     step = 0
