7690854141a7bedf2d61746f25c878de962c86f4
Mathieu Lavoie <mathieulavoie020@gmail.com>
2013-12-27 19:47:28 -0500

    Rail is now in quater step

--- code/src/collector/rail.py
+++ code/src/collector/rail.py
@@ -7,10 +7,9 @@ class Rail(Component):
     LEFT = 1
     RIGHT = 0
     AWAY_POSITION = 0
-    HOME_POSITION = 16700
-    WAIT_FOR_SORTING_POSITION = HOME_POSITION - 600
+    HOME_POSITION = 16700 /2
+    WAIT_FOR_SORTING_POSITION = HOME_POSITION - (600/2)
     CM_PER_STEP = 0.0024765
-    AVERAGE_SPEED = 3/HOME_POSITION
     
     def __init__(self):
         super(Rail, self).__init__(self.state_sorting_position)
