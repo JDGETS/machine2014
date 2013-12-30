449000b74078385ae25fa21d1192b2d937b41ed5
Israel Hallé <isra017@gmail.com>
2013-12-30 17:37:22 -0500

    Document pin config

--- code/src/lib/config.py
+++ code/src/lib/config.py
@@ -1,16 +1,19 @@
 devices = {
+    # Used by collector/sorter
     "white_piston": {
         "pin": "P9_22", #Servo1
         "pull_duty": 5.6,
         "standby_duty": 6.3,
         "push_duty": 7.7
     },
+    # Used by collector/sorter
     "orange_piston": {
         "pin": "P9_14", #Servo0
         "pull_duty": 5.6,
         "standby_duty": 6.3,
         "push_duty": 7.7
     },
+    # Used by collector/vacuum_shaker
     "vacuum_servo": {
         "pin": "P8_13", #Servo2
         "complete_standby_duty": 8.5,
@@ -18,12 +21,14 @@ devices = {
         "pull_duty": 4.0,
         "push_duty": 2.8 #NE PAS DESCENDRE PLUS BAS. Monter PULL_DUTY/COMPLETE_STANDBY_DUTY a la place.
     },
+    # Used by collector/collector_controller
     "gate_servo": {
-        "pin": "P9_42", #Servo3        
+        "pin": "P9_42", #Servo3
         "pull_duty": 6.6,
         "standby_duty": 6.6,
-        "push_duty": 7.9 
+        "push_duty": 7.9
     },
+    # Used by collector/sorter
     "color_sensor": {
         "a_pin": "P9_39", #RGB1
         "b_pin": "P9_40",
@@ -32,63 +37,78 @@ devices = {
         "white_val": [0.065244445204734797, 0.064688889533281321, 0.098844445347785956],
         "orange_val": [0.023955555856227873, 0.073466666638851166, 0.078111109435558324]
     },
-    "spawn_switch": {
-        "pin": "P8_12" # In2
-    },
+    # Used by collector/collector_controller
     "start_collect_switch": {
         "pin": "P8_16" # In4
     },
+    # Used by collector/vacuum_shaker
     "load_tank_switch": {
         "pin": "P8_14" # In3
     },
-    "collector_foot_up":{
-         "pin": "P8_15"
-     },
-    "collector_foot_down":{
-         "pin": "P8_xx"
-     },
-    "stepper_rail":{
-        "pin":"GPIO2_6",
-        "direction":"P8_43",
-        "reset":"P8_44",
-        "enable":"P8_46",
+    # Used by collector/collector_controller
+    "collector_foot_up": {
+         "pin": "P8_15" # Magnetic In2
+    },
+    "collector_foot_down": {
+         "pin": "P8_11" # Magnetic In1
+    },
+    # Used by collector/rail
+    "stepper_rail": {
+        "pin":"GPIO2_6",    # Stepper1 Step
+        "direction":"P8_43",# Stepper1 Dir
+        "reset":"P8_44",    # Stepper1 Reset
+        "enable":"P8_46",   # Stepper1 Enable
+
         "ramp_step":500,
         'min_sleep':175
-     },
-     "rail":{
-         "switch_away": "P8_22",
-         "switch_home": "P8_18",
-         "switch_stand":"P8_15"
-     },
-    "camion":{
+    },
+     # Used by collector/rail
+    "rail": {
+        "switch_away": "P8_22", # In7
+        "switch_home": "P8_18", # In5
+    },
+
+
+    "camion": {
         "foot_standby_time":"3",
         "stepper_start_position_ticks": 3000/4,
         "stepper_foot_complete_ticks": 41000/4, #20.5' <- Distance foot to floor
-     },
-    "camion_collector_switch":{
-        "pin":"P8_18",
-     },
-     "camion_rf_switch":{
-        "pin":"P8_22",
-     },
-    "camion_dump_switch":{
-        "pin":"P8_xx",
-     },
-    "camion_in_position_switch":{
-        "pin":"P8_12",
-     },
-    "camion_foot_up_switch":{
-        "pin":"P8_14", #in3
-     },
-    "camion_foot_down_switch":{
-        "pin":"P8_16", #in4
-     },
-    "camion_stepper":{
-        "pin":"GPIO2_6",
-        "direction":"P8_43",
-        "reset":"P8_44",
-        "enable":"P8_46",
+    },
+    # Used by camion/camion
+    "camion_collector_switch": {
+        "pin":"P8_18", # In5
+    },
+    # Used by camion/camion
+     "camion_rf_switch": {
+        "pin":"P8_22", # In7
+    },
+    # Used by camion/camion
+    "camion_dump_switch": {
+        "pin":"P8_11", # Magnetic In1
+    },
+    # Used by camion/camion
+    "camion_in_position_switch": {
+        "pin":"P8_12", # In2
+    },
+    # Used by camion/camion
+    "camion_foot_up_switch": {
+        "pin":"P8_14", # In3
+    },
+    # Used by camion/camion
+    "camion_foot_down_switch": {
+        "pin":"P8_16", # In4
+    },
+    "camion_stepper": {
+        "pin":"GPIO2_6",    # Stepper1 Step
+        "direction":"P8_43",# Stepper1 Dir
+        "reset":"P8_44",    # Stepper1 Reset
+        "enable":"P8_46",   # Stepper1 Enable
         "ramp_step":2000,
         'min_sleep':120
     },
+
+    # Used by tools/process_spawner. Not used anymore.
+    "spawn_switch": {
+        "pin": "P8_12" # In2
+    }
 }
