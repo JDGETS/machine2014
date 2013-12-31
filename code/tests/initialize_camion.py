a08a2782cb21e381e675d16babcb521c40051d21
Mathieu Binette <mbinette91@gmail.com>
2013-12-30 23:10:16 -0500

    ninja

--- code/tests/initialize_camion.py
+++ code/tests/initialize_camion.py
@@ -7,12 +7,18 @@ camion = Camion()
 while True:
     print "1 : Goto X up (lift foot)"
     print "2 : Goto X down (drop foot)"
+    print "3 : Drop foot"
+    print "4 : Lift foot"
     c = int(raw_input("your choice?"))
   
     if c == 1:
         c = int(raw_input("how much? "))
-        camion.foot_stepper.move(camion.LIFT_FOOT_DIRECTION,abs(c))
+        camion.foot.stepper.move(camion.LIFT_FOOT_DIRECTION,abs(c))
     elif c == 2:
         c = int(raw_input("how much? "))
-        camion.foot_stepper.move(camion.DROP_FOOT_DIRECTION,abs(c))
+        camion.foot.stepper.move(camion.DROP_FOOT_DIRECTION,abs(c))
+    elif c == 3:
+        camion.drop_foot()
+    elif c == 4:
+        camion.bring_foot_up()
 
