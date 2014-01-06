32090ba5c6bcb4787864950e5ea6b520ff2aeff2
Israel Hallé <isra017@gmail.com>
2014-01-05 23:23:47 -0500

    main_collector try again on exception

--- code/src/main_collector.py
+++ code/src/main_collector.py
@@ -1,15 +1,22 @@
 #! /usr/bin/env python
 from collector.collector import Collector
 import sys, os
+import time
 
 if __name__ == "__main__":
     c = None
-    try:
-        print "[main_collector] Start collector"
-        c = Collector()
-        c.run()
+    is_running = True
 
-    finally:
-        if c:
-            c.stop()
-        print "[main_collector] Stop collector"
+    while is_running:
+        try:
+            print "[main_collector] Start collector"
+            c = Collector()
+            c.run()
+            is_running = False
+        except Exception as ex:
+            "[main_collector] Exception catched:", ex
+            time.sleep(10)
+        finally:
+            if c:
+                c.stop()
+            print "[main_collector] Stop collector"
