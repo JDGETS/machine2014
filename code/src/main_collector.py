65b4342500a7c16bced9c50adfa94d19138f68e4
Israel Hallé <isra017@gmail.com>
2014-01-05 23:39:35 -0500

    Restart python script on failure

--- code/src/main_collector.py
+++ code/src/main_collector.py
@@ -1,22 +1,15 @@
 #! /usr/bin/env python
 from collector.collector import Collector
 import sys, os
-import time
 
 if __name__ == "__main__":
     c = None
-    is_running = True
+    try:
+        print "[main_collector] Start collector"
+        c = Collector()
+        c.run()
 
-    while is_running:
-        try:
-            print "[main_collector] Start collector"
-            c = Collector()
-            c.run()
-            is_running = False
-        except Exception as ex:
-            "[main_collector] Exception catched:", ex
-            time.sleep(10)
-        finally:
-            if c:
-                c.stop()
-            print "[main_collector] Stop collector"
+    finally:
+        if c:
+            c.stop()
+        print "[main_collector] Stop collector"
